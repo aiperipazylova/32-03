d27b7594a7da0b058d912907d8407b48884eb976
aiperipazylova <peripro96@gmail.com>
2023-11-21 20:32:07 +0100

    Homework-5 (1/2/3)

--- DZ5_P.Aiperi_32-3/app.js
+++ DZ5_P.Aiperi_32-3/app.js
@@ -1,11 +1,11 @@
 // Задание-1
 
-var arrayNum = [23, 23, 34, 4, 5, 45, 56, 67, 55, 55, 44, 34, 33, 67, 66, 6, 6, 8, 7, 9]
+let arrayNum = [23, 23, 34, 4, 5, 45, 56, 67, 55, 55, 44, 34, 33, 67, 66, 6, 6, 8, 7, 9]
 
 function removeDuplicates(arrayNum) {
   let result = []
   
-  for (var i = 0; i < arrayNum.length; i++) {
+  for (let i = 0; i < arrayNum.length; i++) {
     if (!result.includes(arrayNum[i])) {
       result.push(arrayNum[i])
     }
@@ -37,7 +37,36 @@ const booksForFilter = (bookArray) => {
   console.log(textBooksWithoutU)
   
 }
+
 booksForFilter(books)
 
 
 //Задание-3
+
+let users = [
+  { name: 'Aiperi', age: 26 },
+  { name: 'Ularbek', age: 27 },
+  { name: 'Nurayim', age: 21 },
+  { name: 'Dastan', age: 25 },
+  { name: 'Askar', age: 28 },
+  { name: 'Zhanat', age: 22 },
+  { name: 'Diana', age: 32 },
+  { name: 'Daut', age: 29 }
+]
+
+let youngestUser = users[0]
+let oldestUser = users[0]
+
+for (let i = 1; i < users.length; i++) {
+  if (users[i].age < youngestUser.age) {
+    youngestUser = users[i]
+  }
+
+  if (users[i].age > oldestUser.age) {
+    oldestUser = users[i]
+  }
+}
+
+let ageDifference = oldestUser.age - youngestUser.age
+
+console.log(`Пользователь ${oldestUser.name} старше ${youngestUser.name} на ${ageDifference} лет.`)
